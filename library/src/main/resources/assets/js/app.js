e904fc1350f831992c91e41e911eff46c0016588
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2013-11-14 09:33:49 -0800

    Commit!

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,14 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+        var isbn = this.id;
+        var puturl = 'http://localhost:8001/library/v1/books/'+isbn+'?status=lost';
+        var btnisbn = ':button#'+isbn;
+        $.ajax({
+                  url: puturl,
+                  type: 'PUT',
+                  success: function(data) {
+                          alert('About to report lost on ISBN ' + isbn);
+                          location.reload();
+                  }
+                });
+        $(btnisbn).attr("disabled",true);
 });
