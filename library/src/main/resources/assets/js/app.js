e51bfaa1e217dcaaa3cf90863cd0229622437b00
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2013-11-14 13:25:31 -0800

    Final Commit

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,14 +1,39 @@
 $(":button").click(function() {
         var isbn = this.id;
-        var puturl = 'http://localhost:8001/library/v1/books/'+isbn+'?status=lost';
-        var btnisbn = ':button#'+isbn;
-        $.ajax({
-                  url: puturl,
+        var url1 = "v1/books/"+isbn+"?status=lost"
+        var buttonNow = ':button#'+isbn;
+        
+                  $.ajax({
+                  url: url1 ,
                   type: 'PUT',
                   success: function(data) {
                           alert('About to report lost on ISBN ' + isbn);
-                          location.reload();
+                         
+                         location.reload();
+                         
                   }
+               
                 });
-        $(btnisbn).attr("disabled",true);
-});
+               
+              $(buttonNow).attr("disabled", true);
+});    
+
+  /* on page reload */    
+$(document).ready(function() {
+    var books = document.getElementById("books");
+    for (var i = 0, row; row = books.rows[i]; i++) {
+            var bookStatus=books.rows[i].cells[4].innerHTML;
+            if (bookStatus=="lost")
+            {
+                    var newIsbn=books.rows[i].cells[0].innerHTML;
+                    document.getElementById(newIsbn).disabled = true;
+            }
+    }
+
+	
+});    
+
+
+
+
+
